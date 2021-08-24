cc92e8100524ab821bf9cd88b826680408cd04d8
mandarinka99 <alina.oksentiuk@gmail.com>
2021-08-24 23:03:03 +0300

    TransactionsList styles

--- src/Components/transactionHistory/TransactionHistory.js
+++ src/Components/transactionHistory/TransactionHistory.js
@@ -1,26 +1,29 @@
 import PropTypes from "prop-types";
+import s from "./TransactionHistory.module.css";
 
 const TransactionHistory = ({ items }) => {
   return (
-    <table class="transaction-history">
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        {items.map(({ id, type, amount, currency }) => (
-          <tr key={id}>
-            <td>{type}</td>
-            <td>{amount}</td>
-            <td>{currency}</td>
+    <div className={s.transactionHistory}>
+      <table className={s.table}>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+
+        <tbody>
+          {items.map(({ id, type, amount, currency }) => (
+            <tr key={id}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
